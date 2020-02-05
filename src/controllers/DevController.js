89aff602458dff316d2aff5345f64b070fc0e89a
marion <marcosmariondev@gmail.com>
2020-02-04 22:15:03 -0300

    update api

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,31 +1,40 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+    return response.json(devs);
+  },
+
   async store(request, response) {
-    console.log(request.body);
     const { github_username, techs, latitude, longitude } = request.body;
-    console.log(request.body);
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
-    let { name = login, avatar_url, bio } = apiResponse.data;
-    const techsArray = techs.split(",").map(tech => tech.trim());
+    let dev = await Dev.findOne({ github_username });
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      let { name = login, avatar_url, bio } = apiResponse.data;
+      const techsArray = parseStringAsArray(techs);
+
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return response.json(dev);
   }
